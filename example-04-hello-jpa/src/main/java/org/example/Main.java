f465206f32ce705384e029551cc2a5dcb02cf63d
ppzxc <cjh8487@naver.com>
2022-11-17 23:28:44 +0900

    2022-11-17 add hello jpa

---
+++ example-04-hello-jpa/src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.example.entity.Member;
+
+public class Main {
+
+  public static void main(String[] args) {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+    EntityManager em = emf.createEntityManager();
+    EntityTransaction tx = em.getTransaction();
+    tx.begin();
+
+    Member member = new Member();
+    member.setId(1L);
+    member.setName("조정하");
+
+    em.persist(member);
+
+    tx.commit();
+    em.close();
+    emf.close();
+  }
+}
